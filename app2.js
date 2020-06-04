9e7231c1b8c97564bda85b87db639e9b8d14579e
HereIsKam <HereIsKam@users.noreply.github.com>
2020-06-04 13:15:38 +0300

    Corrects

--- app2.js
+++ app2.js
@@ -1,70 +1,88 @@
 'use strict'
+
 const data1 = data;
-const trackList = document.getElementById('track-list');
+const goodsList = document.getElementById('goods-list');
 console.log(data);
 
-appendCards(data);
+
+
+data1.sort(sortedPriceIncrease(data1.price, ))
+
+
+appendCards(data1);
+
+function sortedPriceIncrease(a, b) {
+  if (a > b) return 1;
+  if (a == b) return 0;
+  if (a < b) return -1;
+}
+
+
 
 function renderSortedCards(new_data) {
-  trackList.innerHTML = '';
+  goodsList.innerHTML = '';
   appendCards(new_data);
 }
 
-
-function appendCards(tracks) {
-  for (let i = 0; i < tracks.length; i++) {
-    const song = tracks[i];
-    let template = createCardTemplate(song);
-    trackList.insertAdjacentHTML('beforeend',template);
+// Creating card list
+function appendCards(goods) {
+  for (let i = 0; i < goods.length; i++) {
+    const product = goods[i];
+    let template = createCardTemplate(product);
+    goodsList.insertAdjacentHTML('beforeend', template);
   }
 }
-
-function createCardTemplate(track) {
+//Sort by increasing price
+function sortIncreasePrice(goods) {
+  return goods.price
+}
+// Star rating
+function createCardTemplate(goods) {
   let stars = ``;
 
   for (let i = 1; i <= 5; i++) {
-    if (i <= +track.comments_mark) {
+    if (i <= +goods.comments_mark) {
       stars += '&#9733;'
     } else {
       stars += '&#9734;'
     }
   }
 
-  let sell_status_text = ``;  
-  let sell_status_class = ``; 
-  if (track.sell_status == 'available') {
-    sell_status_text = `В наличии`;  
-    sell_status_class = `green`; 
-  } else if (track.sell_status == 'limited'){
-    sell_status_text = `Заканчивается`;  
-    sell_status_class = `yellow`; 
+  // Status color
+  let sell_status_text = ``;
+  let sell_status_class = ``;
+  if (goods.sell_status == 'available') {
+    sell_status_text = `В наличии`;
+    sell_status_class = `green`;
+  } else if (goods.sell_status == 'limited') {
+    sell_status_text = `Заканчивается`;
+    sell_status_class = `yellow`;
+  } else {
+    sell_status_text = `Нет в наличии`;
+    sell_status_class = `red`;
   }
-  else {
-    sell_status_text = `Нет в наличии`;  
-    sell_status_class = `red`; 
-  }
-  
+  // Card body
   let html = `<div class="col mb-3">
     <div class="card track">
-      <img src="${track.images.preview}" class="card-img-top" alt="Artwork ${track.title} - ${track.brand}" />
+      <img src="${goods.images.preview}" class="card-img-top" alt="Artwork ${goods.title} - ${goods.brand}" />
       <div class="card-body">
-        <h5 class="card-title card-name">${track.title}</h5>
+        <h5 class="card-title card-name">${goods.title}</h5>
         <h6 class="card-title card-rating">Rating: ${stars}</h6>
-        ${(track.brand) ? `<h6 class="card-title card-brand">Brand: ${track.brand}</h6>` : ``}
+        ${(goods.brand) ? `<h6 class="card-title card-brand">Brand: ${goods.brand}</h6>` : ``}
         
 
-        ${(track.docket) ? `<p class="card-desc">${track.docket}</p>` : ``}
+        ${(goods.docket) ? `<p class="card-desc">${goods.docket}</p>` : ``}
         
-        <h6 class="card-title card-category">Categoty: ${track.category_title || 'Ноутбук'}</h6>
-        <a class="card-link" href="${track.href}">View on Rozetka:</a>
+        <h6 class="card-title card-category">Categoty: ${goods.category_title || 'Ноутбук'}</h6>
+        <a class="card-link" href="${goods.href}">View on Rozetka:</a>
         <h6 class="card-title card-status ${sell_status_class}">${sell_status_text}</h6>
 
-        ${(track.old_price != 0) ? `<h6 class="card-title card-price-old">Old price ${track.old_price} UAH</h6> ` : ``}
+        ${(goods.old_price != 0) ? `<h6 class="card-title card-price-old">Old price ${goods.old_price} UAH</h6> ` : ``}
         
-        <h6 class="card-title card-price">New price: ${track.price} UAH</h6>
-        <h6 class="card-title card-discount">Discount: ${track.discount}%</h6>
-        <h6 class="card-title card-id">Item Number: ${track.id}</h6>
-        <h6 class="card-title card-comments">Comments: ${track.comments_amount}</h6>
+        <h6 class="card-title card-price">New price: ${goods.price} UAH</h6>
+        <h6 class="card-title card-discount">Discount: ${goods.discount}%</h6>
+        <h6 class="card-title card-id">Item Number: ${goods.id}</h6>
+        <h6 class="card-title card-comments">Comments: ${goods.comments_amount}</h6>
         <button class="btn btn-success buy-btn">Buy</button>
       </div>
     </div>
@@ -74,14 +92,14 @@ function createCardTemplate(track) {
 }
 
 
-console.dir(btn);
+/* console.dir(btn);
 
 btn.style.backgroundColor = 'red';
 console.log(btn.style.backgroundColor);
 
 
 
-trackList.addEventListener('click', function (e) {
+goodsList.addEventListener('click', function (e) {
   if (e.target.classList.contains('buy-btn')) {
     console.log('YES');
   }
@@ -94,10 +112,10 @@ trackList.addEventListener('click', function (e) {
 
 selectSort.addEventListener('change', function (e) {
   console.log(e.target.value);
-  
+
 })
 
-
+ */
 
 
 
@@ -121,5 +139,5 @@ selectSort.addEventListener('change', function (e) {
 
 // f('click',function (bla) {
 //   console.log(bla);
-  
+
 // });
