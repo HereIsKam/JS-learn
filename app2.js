be541c3fd2e2d4a0ad99bed15038e223d89ce8b1
HereIsKam <HereIsKam@users.noreply.github.com>
2020-06-12 18:01:12 +0300

    Basket

--- app2.js
+++ app2.js
@@ -124,8 +124,8 @@ function appendPriceRange(cards) {
   });
   console.log(prices.sort());
 
-  const template = createPriceRange(prices);
-  filterPrice.insertAdjacentHTML("beforeend", template);
+  //const template = createPriceRange(prices);
+  //filterPrice.insertAdjacentHTML("beforeend", template);
 }
 
 //Creating price filter's range
@@ -185,41 +185,41 @@ function createCardTemplate(products) {
 
   // Card body
   let html = `<div class="col mb-3">
-    <div class="card track">
+    <div class="card track" data-id="${products.id}">
       <img src="${products.images.preview}" class="card-img-top" alt="Artwork ${
     products.title
-  } - ${products.brand}" />
+    } - ${products.brand}" />
       <div class="card-body">
         <h5 class="card-title card-name">${products.title}</h5>
         <h6 class="card-title card-rating">Rating: ${stars}</h6>
         ${
-          products.brand
-            ? `<h6 class="card-title card-brand">Brand: ${products.brand}</h6>`
-            : ``
-        }
+    products.brand
+      ? `<h6 class="card-title card-brand">Brand: ${products.brand}</h6>`
+      : ``
+    }
         ${products.docket ? `<p class="card-desc">${products.docket}</p>` : ``}
         <h6 class="card-title card-category">Categoty: ${
-          products.category_title || "Ноутбук"
-        }</h6>
+    products.category_title || "Ноутбук"
+    }</h6>
         <a class="card-link" href="${products.href}">View on Rozetka:</a>
         <h6 class="card-title card-status ${sell_status_class}">${sell_status_text}</h6>
         ${
-          products.old_price != 0
-            ? `<h6 class="card-title card-price-old">Old price ${products.old_price} UAH</h6> `
-            : ``
-        }
-        <h6 class="card-title card-price">New price: ${products.price} UAH</h6>
+    products.old_price != 0
+      ? `<h6 class="card-title card-price-old">Old price ${products.old_price} UAH</h6> `
+      : ``
+    }
+        <h6 class="card-title card-price" data-price="${products.price}">New price: ${products.price} UAH</h6>
         <h6 class="card-title card-discount">Discount: ${
-          products.discount
-        }%</h6>
+    products.discount
+    }%</h6>
         <h6 class="card-title card-id">Item Number: ${products.id}</h6>
         <h6 class="card-title card-comments">Comments: ${
-          products.comments_amount
-        }</h6>
+    products.comments_amount
+    }</h6>
         <button class="btn btn-success buy-btn">Buy</button>
         <button class="btn btn-primary wish-btn" data-id="${
-          products.id
-        }">&#10084;</button>
+    products.id
+    }">&#10084;</button>
       </div>
     </div>
   </div>`;
@@ -228,3 +228,80 @@ function createCardTemplate(products) {
 
 let str111 =
   'Экран 15.6" IPS (1920x1080) Full HD, матовый / Intel Core i5-9300H (2.4 - 4.1 ГГц) / RAM 8 ГБ / SSD 256 ГБ / nVidia GeForce GTX 1650, 4 ГБ / без ОД / LAN / Wi-Fi / Bluetooth / веб-камера / DOS / 2.32 кг / черный';
+
+
+
+productsList.addEventListener("click", function (e) {
+  if (e.target.classList.contains("buy-btn")) {
+    const btn = e.target;
+    btn.classList.add('disabled')
+    btn.disabled = true
+    btn.textContent = 'В корзине'
+    const card = btn.closest('.card');
+    const img = card.querySelector('.card-img-top')
+    const title = card.querySelector('.card-name')
+    const price = card.querySelector('.card-price')
+
+    const product = {};
+    product.id = card.dataset.id;
+    product.img = img.src;
+    product.title = title.textContent;
+    product.price = price.dataset.price
+
+    renderCartRow(cartBody, product);
+    refreshCartRowNumber(cartBody)
+  }
+});
+cartBody.addEventListener('input', function (e) {
+  if (e.target.classList.contains('count-input')) {
+    const input = e.target
+    if (input.value < 1) {
+      input.value = 1
+    } else if (input.value > 99){
+      input.value = 99
+    }
+    const parentRow = input.closest('.cart-row');
+    let price = parentRow.querySelector('.cart-row-price').dataset.price
+    const sum = parentRow.querySelector('.cart-row-sum')
+    sum.textContent = +input.value * +price
+  }
+})
+cartBody.addEventListener("click", function (e) {
+  if (e.target.classList.contains("cart-row-remove-btn")) {
+    const parentRow = e.target.closest('.cart-row');
+    parentRow.remove();
+    refreshCartRowNumber(this)
+    let id = parentRow.dataset.id;
+    const card = productsList.querySelector(`.card[data-id="${id}"]`)
+    const cardBuyBtn = card.querySelector('.buy-btn')
+    cardBuyBtn.textContent = 'Buy'
+    cardBuyBtn.disabled = false
+    cardBuyBtn.classList.remove('disabled')
+  }
+})
+function renderCartRow(id, data) {
+  let template = `<div class="row cart-row mb-3" data-id="${data.id}">
+    <div class="col-1 cart-row-number">1</div>
+    <div class="col-1 cart-row-img">
+      <img src="${data.img}" alt="${data.title}" class="img-fluid">
+    </div>
+    <h6 class="col-4 cart-row-title">${data.title}</h6>
+    <div class="col-2 cart-row-price" data-price="${data.price}">${data.price}</div>
+    <div class="col-1 cart-row-count">
+      <input class="count-input" type="number" value="1" min="1" max="99">
+    </div>
+    <div class="col-2 cart-row-sum">${data.price}</div>
+    <div class="col-1 cart-row-remove">
+      <button class="btn btn-danger btn-sm cart-row-remove-btn">&times;</button>
+    </div>
+  </div>`;
+  id.insertAdjacentHTML('beforeend', template)
+}
+function refreshCartRowNumber(id) {
+  const rows = id.children
+  for (let i = 0; i < rows.length; i++) {
+    const element = rows[i];
+    element.querySelector('.cart-row-number').textContent = i+1
+  }
+}
+
